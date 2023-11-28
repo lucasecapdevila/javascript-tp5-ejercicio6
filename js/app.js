c735e8ef1914c18254fd0935d0d7797e116ec5e7
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-27 22:31:11 -0300

    se crea función para convertir milisegundos, cargar sonido y mostrar/ocultar elementos

--- js/app.js
+++ js/app.js
@@ -12,3 +12,37 @@ const contenedor = document.getElementById('contenedorInputs'),
 let idInterval = null,
     fechaFutura = null,
     diferenciaDeTiempo = 0
+
+
+
+//  Funciones globales
+const mostrarElemento = (el) => {
+  el.classList.remove('d-none')
+}
+
+const ocultarElemento = (el) => {
+  el.classList.add('d-none')
+}
+
+const cargarSonido = (sonido) => {
+  const audio = document.createElement('audio')
+  audio.src = sonido
+  audio.loop = true
+  audio.setAttribute('preload', 'auto')
+  audio.setAttribute('controls', 'none')
+  document.body.appendChild(audio)
+  ocultarElemento(audio)
+  return audio
+}
+
+const audio = cargarSonido('media/alarma.wav')
+
+
+
+//  Funciones del temporizador
+const convertirMilisegundos = (milisegundos) => {
+  const minutos = parseInt(milisegundos / 1000 / 60)
+  milisegundos -= minutos * 60 * 1000
+  segundos = (milisegundos / 1000)
+  return `${minutos}:${segundos}`
+}
