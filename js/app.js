aa9b597475c34a4beccf0a1e6fd2a02a48f448b4
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-28 11:13:11 -0300

    funciones para temporizador terminadas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 //  Variables
 const contenedor = document.getElementById('contenedorInputs'),
-      dias = document.getElementById('inputDias'),
-      horas = document.getElementById('inputHoras'),
-      minutos = document.getElementById('inputMinutos'),
-      segundos = document.getElementById('inputSegundos'),
+      diasUser = document.getElementById('inputDias'),
+      horasUser = document.getElementById('inputHoras'),
+      minutosUser = document.getElementById('inputMinutos'),
+      segundosUser = document.getElementById('inputSegundos'),
       tiempoRestante = document.getElementById('tiempoRestante'),
       btnInicio = document.getElementById('btnInicio'),
       btnPausa = document.getElementById('btnPausa'),
@@ -15,15 +15,7 @@ let idInterval = null,
 
 
 
-//  Funciones globales
-const mostrarElemento = (el) => {
-  el.classList.remove('d-none')
-}
-
-const ocultarElemento = (el) => {
-  el.classList.add('d-none')
-}
-
+//  Funciones
 const cargarSonido = (sonido) => {
   const audio = document.createElement('audio')
   audio.src = sonido
@@ -31,23 +23,20 @@ const cargarSonido = (sonido) => {
   audio.setAttribute('preload', 'auto')
   audio.setAttribute('controls', 'none')
   document.body.appendChild(audio)
-  ocultarElemento(audio)
+  audio.style.display = 'none' 
   return audio
 }
-
 const audio = cargarSonido('media/alarma.wav')
 
-
-
-//  Funciones del temporizador
-const convertirMilisegundos = (milisegundos) => {
-  const minutos = parseInt(milisegundos / 1000 / 60)
-  milisegundos -= minutos * 60 * 1000
-  segundos = (milisegundos / 1000)
-  return `${minutos}:${segundos}`
+const ocultarElemento = (el) => {
+  el.classList.add('d-none')
 }
 
-const iniciar = (minutos, segundos) => {
+const mostrarElemento = (el) => {
+  el.classList.remove('d-none')
+}
+
+const iniciarTemporizador = (minutos, segundos) => {
   ocultarElemento(contenedor)
   mostrarElemento(btnPausa)
   ocultarElemento(btnInicio)
@@ -59,18 +48,75 @@ const iniciar = (minutos, segundos) => {
   } else{
     const milisegundos = (segundos + (minutos * 60)) * 1000
     fechaFutura = new Date(new Date().getTime() + milisegundos)
+    console.log(minutos);
   }
-
+  
   clearInterval(idInterval)
   idInterval = setInterval(() => {
-    const tiempoRestante = fechaFutura.getTime() - new Date().getTime()
-    if(tiempoRestante <= 0){
+    const tiempoQueFalta = fechaFutura.getTime() - new Date().getTime()
+    if(tiempoQueFalta <= 0){
       clearInterval(idInterval)
       audio.play()
       ocultarElemento(btnPausa)
       mostrarElemento(btnReinicio)
     } else{
-      tiempoRestante.innerText = convertirMilisegundos(tiempoRestante)
+      tiempoRestante.textContent = convertirMilisegundos(tiempoQueFalta)
     }
-  })
+  }, 50)
 }
+
+const pausarTemporizador = () => {
+  ocultarElemento(btnPausa)
+  mostrarElemento(btnInicio)
+  mostrarElemento(btnReinicio)
+  diferenciaDeTiempo = fechaFutura.getTime() - new Date().getTime()
+  clearInterval(idInterval)
+}
+
+const reiniciarTemporizador = () => {
+  clearInterval(idInterval)
+  fechaFutura = null
+  diferenciaDeTiempo = 0
+  audio.currentTime = 0
+  audio.pause()
+  tiempoRestante.textContent = '00:00.0'
+  inicioApp()
+}
+
+const agregarCero = (valor) => {
+  if(valor < 10){
+    return '0' + valor
+  } else{
+    return '' + valor
+  }
+}
+
+const convertirMilisegundos = (milisegundos) => {
+  const minutos = parseInt(milisegundos / 1000 / 60)
+  milisegundos -= minutos * 60 * 1000
+  segundos = (milisegundos / 1000)
+  return `${agregarCero(minutos)}:${agregarCero(segundos.toFixed(1))}`
+}
+
+const inicioApp = () => {
+  minutosUser.value = '0'
+  segundosUser.value = '0'
+  mostrarElemento(contenedor)
+  mostrarElemento(btnInicio)
+  ocultarElemento(btnPausa)
+  ocultarElemento(btnReinicio)
+}
+
+btnInicio.addEventListener('click', () => {
+  const minutos = parseInt(minutosUser.value)
+  const segundos = parseInt(segundosUser.value)
+  if(isNaN(minutos) || isNaN(segundos) || (segundos <= 0 && minutos <= 0)){
+    console.log('Error');
+    return
+  } 
+  iniciarTemporizador(minutos, segundos)
+})
+
+inicioApp()
+btnPausa.addEventListener('click', pausarTemporizador)
+btnReinicio.addEventListener('click', reiniciarTemporizador)
